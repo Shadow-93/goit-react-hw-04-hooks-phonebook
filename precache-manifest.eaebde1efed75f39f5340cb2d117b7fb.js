33eea7b5cac0089c10671355fafdbda3c3aaee18
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-12-28 21:33:07 +0200

    Updates

---
+++ precache-manifest.eaebde1efed75f39f5340cb2d117b7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a311b3c4b00a605be79d4a233859764",
+    "url": "/goit-react-hw-04-hooks-phonebook/index.html"
+  },
+  {
+    "revision": "c00b0b29119c59672679",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/css/main.97f6b757.chunk.css"
+  },
+  {
+    "revision": "e97ed223a747d207f5c8",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.665d8cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.665d8cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00b0b29119c59672679",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/main.3b323934.chunk.js"
+  },
+  {
+    "revision": "4dcff631daeb49597abe",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/runtime-main.79e560b6.js"
+  }
+]);
